3d8215cb7bac2f170386c99a185b278fbcc21583
maciejbandyk <maciejbandyk@gmail.com>
2019-06-28 15:32:10 +0200

    Created addNewToolbar function

--- widget.js
+++ widget.js
@@ -4,14 +4,32 @@ class ToolbarComponent {
     this.position = position;
   }
 
-  draw() {
-    var toolbar = document.getElementById('navigation');
-    toolbar.innerHTML = this.position;
-  }
+  addNewToolbar() {
 
+      var naviWrapper = document.createElement("div");
+      var message = document.createElement("span");
+      var button = document.createElement("button");
 
+      button.innerText = "Get Widgets";
+      button.className = "button";
+      button.setAttribute("id", "Btn");
+      naviWrapper.className = "navigation-wrapper";
+      message.innerText = this.msg;
+      message.className = "text";
+
+      var bar = document.getElementById('navigation');
+      bar.appendChild(naviWrapper);
+      naviWrapper.appendChild(message);
+      naviWrapper.appendChild(button);
+    }
 }
 //tests
-let tolbar = new ToolbarComponent("asdasd", 2);
-tolbar.draw();
-//tolbar.wyswietl();
+
+
+
+let tolbar = new ToolbarComponent("Add beautiful widgets to your website", "top");
+tolbar.addNewToolbar();
+
+document.getElementById("Btn").addEventListener("click", function(){
+  alert("Hello world!");
+});
