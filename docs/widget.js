46541504432509d8bc2b6047379012d0ecfb8d51
maciejbandyk <maciejbandyk@gmail.com>
2019-07-09 04:10:45 +0200

    Test folder with widget

---
+++ docs/widget.js
@@ -0,0 +1,50 @@
+class TaskBar {
+  constructor(options) {
+    this.message = options.message || "Some default message";
+    this.position = options.position || "top";
+    this.buildWidget();
+    this.renderDomContent();
+
+  }
+
+  buildWidget() {
+    this.wrapper = document.createElement('div');
+    this.wrapper.className= "component-wrapper "+ this.position;
+
+    this.markup = `<span class="task-message"></span>
+    <button class="task-button">Click Me!</button>
+    <div class="task-close"></div>`;
+
+    this.wrapper.innerHTML = this.markup;
+
+    this.msg = this.wrapper.getElementsByClassName('task-message').item(0);
+    this.msg.innerText = this.message;
+    this.btn = this.wrapper.getElementsByClassName('task-button').item(0);
+    this.cls = this.wrapper.getElementsByClassName('task-close').item(0);
+    this.btn.onclick = () => alert('test');
+    this.cls.onclick = () => TaskBar.fadeOutEffect();
+  }
+
+  renderDomContent() {
+    document.body.append(
+      this.wrapper
+    );
+  }
+
+  static fadeOutEffect() {
+    var fadeTarget = document.getElementsByClassName("component-wrapper").item(0);
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 20);
+  }
+
+}
+
+let taskBar = new TaskBar('wiadomosc','bottom');
